92a0dfd9096eac66b45f236c7b7f9d72311c6f85
btubay <btubay@gmail.com>
2017-03-16 10:34:21 +0000

    commit cs push :D

--- src/main/java/consuinter/App.java
+++ src/main/java/consuinter/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World modified via cs push!" );
+        System.out.println( "Hello World modified via cs push :D" );
 
     }
 }
